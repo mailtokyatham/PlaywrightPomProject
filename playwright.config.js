81a63e54ef7e5b5fc7d61efb9cfdd67beabf5b83
Srinivas <mailtokyatham@gmail.com>
2024-09-16 17:09:48 +1000

    config changes

--- playwright.config.js
+++ playwright.config.js
@@ -31,15 +31,17 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless : true,
     baseURL : 'https://www.lambdatest.com/',
-    screenshot:'on',
-    video: 'off',
+    screenshot:'only-on-failure',
+    video: 'retain-on-failure',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  // testMatch : ["tests/script/inputFormSubmit.test.js"],
 
   /* Configure projects for major browsers */
   projects: [
