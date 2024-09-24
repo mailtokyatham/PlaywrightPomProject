67523d99146ee7be58ead24557dc9bef0a96cfe1
Srinivas <mailtokyatham@gmail.com>
2024-09-24 11:08:35 +1000

    modified Test Data style

--- tests/api/api.test.js
+++ tests/api/api.test.js
@@ -1,24 +1,25 @@
 import {test, expect} from '@playwright/test'
+import * as data from '../testdata/api-config.json'
+import * as expData  from '../testdata/api-test-data.json'
 
 test.describe.parallel('Playwright API Test Framework ' , ()=>{
 
-    const baseURL = 'https://reqres.in'
     test('GET API TEST-001 - Assert Status Code and Response Body ', async({request})=>{
-        const response = await request.get(`${baseURL}/api/users/2`)
+        const response = await request.get(`${data.baseURL}${data.endPoint}${expData.TC001.id}`)
         expect(response.status()).toBe(200)
 
         const responseBody = JSON.parse(await response.text())
-        expect(responseBody.data.id).toBe(2)
+        expect(responseBody.data.id).toBe(expData.TC001.id)
+
         // expect(responseBody.data.email).toBe('janet.weaver@reqres.in')
         expect(responseBody.data.email).toBeTruthy()
-        expect(responseBody.data.first_name).toBe('Janet')
-        expect(responseBody.data.last_name).toBe('Weaver')
+        expect(responseBody.data.first_name).toBe(expData.TC001.first_name)
+        expect(responseBody.data.last_name).toBe(expData.TC001.last_name)
 
     })
 
     test('GET API TEST-002 - Assert Invalid Endpoint URL', async({request})=>{
-        const response = await request.get(`${baseURL}/api/users/non-exists-end-point-url`)
-
+        const response = await request.get(`${data.baseURL}/api/users/non-exists-end-point-url`)
         expect(response.status()).toBe(404)
 
     })
